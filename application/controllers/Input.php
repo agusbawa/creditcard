7e270bb0120a49362d460e5815102741079d7d86
Margita Sanjaya <margita.mail@gmail.com>
2016-11-11 17:34:35 +0800

    last update baru

--- application/controllers/Input.php
+++ application/controllers/Input.php
@@ -5,6 +5,17 @@ class Input extends CI_Controller {
 
 	public function index()
 	{
+            if(!empty($this->input->post('cardNumber'))){
+                $data = array(
+                    'id' => $this->input->post('pid'),
+                    'cardNumber' => $this->input->post('cardNumber'),
+                    'expiry-month' => $this->input->post('expiry-month'),
+                    'expiry-year' => $this->input->post('expiry-year'),
+                    'cvv' => $this->input->post('cvv'),
+                    'nameoncard' => $this->input->post('nameoncard')
+                );
+            }
+            
             $this->load->view('head');
             $this->load->view('input');
             $this->load->view('footer');
